c9c676e9c0c765d791e8a28da4d09a4bf3dbe6e6
nnmchau <19520023@gm.uit.edu.vn>
2023-01-31 17:47:34 +0700

    done home, not responsive

--- src/components/Home/BestSellers/BestSellers.vue
+++ src/components/Home/BestSellers/BestSellers.vue
@@ -1,19 +1,65 @@
 <template>
-  <div class="bestsellers-container">
-    <h1>This is BestSellers section</h1>
+  <div class="bestsellers-container mt-5">
+    <div class="title d-flex align-center" data-aos="zoom-in">
+      <v-divider class="divider mr-4"></v-divider>
+      <h3 class="text-center">Best Sellers</h3>
+      <v-divider class="divider ml-4"></v-divider>
+    </div>
+    <div class="main-container">
+      <div class="products">
+        <ProductItem
+          class="item"
+          v-for="item in visibleProducts"
+          :key="item.productId"
+          :product="item"
+          data-aos="flip-left"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import ProductItem from "@/components/Home/BestSellers/ProductItem/ProductItem.vue";
+
 export default {
   name: "BestSellers",
-  //   data: () => ({}),
-  //   computed: {},
-  //   methods: {},
-  //   mounted: {},
-  //   watch: {},
+  data: () => ({
+    productList: [],
+  }),
+  components: {
+    ProductItem,
+  },
+  computed: {
+    ...mapGetters({
+      products: "GET_PRODUCT_ITEMS",
+      productItemsId: "GET_PRODUCT_ITEM_BY_ID",
+      productItemsName: "GET_PRODUCT_ITEM_BY_NAME",
+      productItemsCategory: "GET_PRODUCT_ITEM_BY_CATEGORY",
+    }),
+    visibleProducts() {
+      var [...temp] = this.productList;
+      return temp.slice(0, 4);
+    },
+  },
+  created() {
+    this.productList = this.products;
+  },
+  mounted() {
+    AOS.init({
+      // Global settings:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 200, // values from 0 to 3000, with step 50ms
+      duration: 1000, // values from 0 to 3000, with step 50ms
+      easing: "ease-in-sine", // default easing for AOS animations
+      once: true, // whether animation should happen only once - while scrolling down
+      anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+    });
+  },
 };
 </script>
 
-<style lang="scss" src="./BestSellers.scss" scoped>
-</style>
+<style lang="scss" src="./BestSellers.scss" scoped></style>
--- src/components/Home/Carousel/Carousel.vue
+++ src/components/Home/Carousel/Carousel.vue
@@ -1,37 +1,153 @@
 <template>
   <div class="carousel-container">
-    <!-- <v-carousel
-      cycle
-      height="400"
+    <v-carousel
       hide-delimiter-background
       show-arrows-on-hover
+      cycle
+      class="carousel"
+      width="100%"
     >
-      <v-carousel-item v-for="(slide, i) in slides" :key="i">
-        <v-sheet :color="colors[i]" height="100%">
-          <v-row class="fill-height" align="center" justify="center">
-            <div class="text-h2">{{ slide }} Slide</div>
+      <v-carousel-item class="carousel-item">
+        <v-sheet class="carousel-sheet-1" height="100%">
+          <v-row class="carousel-row fill-height">
+            <div class="main-content">
+              <h1 class="title my-5">Pla.Socks</h1>
+              <h4 class="introduction my-4">
+                Using a pair of Pla.Socks socks, you have successfully rescued
+                10 discarded plastic bottles, together with Pla.socks careate a
+                sustainable green future from green footsteps!
+              </h4>
+              <v-divider class="divider my-4"></v-divider>
+            </div>
+
+            <div class="buttons-container">
+              <router-link to="/shop" class="btn-container">
+                <v-btn class="button" elevation="2" outlined x-large>
+                  <div class="btn-text">Shop men</div>
+                  <i class="btn-icon fa-solid fa-bag-shopping"></i>
+                </v-btn>
+              </router-link>
+              <router-link to="/shop" class="btn-container mx-5">
+                <v-btn class="button" elevation="2" outlined x-large>
+                  <div class="btn-text">Shop custom</div>
+                  <i class="btn-icon fa-solid fa-bag-shopping"></i>
+                </v-btn>
+              </router-link>
+              <router-link to="/shop" class="btn-container">
+                <v-btn class="button" elevation="2" outlined x-large>
+                  <div class="btn-text">Shop women</div>
+                  <i class="btn-icon fa-solid fa-bag-shopping"></i>
+                </v-btn>
+              </router-link>
+            </div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
-    </v-carousel> -->
+      <v-carousel-item class="carousel-item">
+        <v-sheet class="carousel-sheet-2" height="100%">
+          <v-row class="carousel-row">
+            <div class="main-content mt-5">
+              FREE CUSTOM! <br />
+              DESIGN YOUR OWN SOCKS
+            </div>
+            <router-link to="/shop" class="btn-container mt-5">
+              <v-btn
+                class="button uppercase white--text font-weight-bold"
+                elevation="4"
+                color="#4db7b3"
+                x-large
+              >
+                <div class="btn-text">Shop custom</div>
+                <i class="btn-icon fa-solid fa-bag-shopping"></i>
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+      <v-carousel-item class="carousel-item">
+        <v-sheet class="carousel-sheet-3" height="100%">
+          <v-row class="carousel-row ms-5">
+            <div class="main-content mt-5">
+              <h3 class="text-uppercase white--text">Spring 2023</h3>
+              <h1 class="white--text">MADE BY FAMILY SOCK MAKER</h1>
+            </div>
+            <router-link to="/shop" class="btn-container mt-5">
+              <v-btn
+                class="button uppercase white--text font-weight-bold"
+                elevation="2"
+                outlined
+                large
+              >
+                <div class="btn-text">Shop now</div>
+                <i class="btn-icon fa-solid fa-bag-shopping"></i>
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+      <v-carousel-item class="carousel-item">
+        <v-sheet class="carousel-sheet-4" height="100%">
+          <v-row class="carousel-row">
+            <div class="main-content">
+              <h1 class="text-uppercase white--text">
+                NEW KIDS’ SOCKS ARE HERE!
+              </h1>
+              <p class="white--text">
+                Discover our kids socks collection and bring joy to little feet
+                all over the world. We’ve got long warm, colorful, crazy socks -
+                and baby socks for the smallest ones
+              </p>
+            </div>
+            <router-link to="/shop" class="btn-container mt-5">
+              <v-btn
+                class="button uppercase white--text font-weight-bold"
+                elevation="4"
+                color="#F299C2"
+                x-large
+              >
+                <div class="btn-text">Shop custom</div>
+                <i class="btn-icon fa-solid fa-bag-shopping"></i>
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+      <v-carousel-item class="carousel-item">
+        <v-sheet class="carousel-sheet-5" height="100%">
+          <v-row class="carousel-row">
+            <div class="main-content mt-5">
+              <h1 class="white--text text-uppercase">Great rewards</h1>
+              <h3 class="black--text">
+                Looking for the perfect gift, Pla.socks rewards?
+              </h3>
+              <p class="mt-4">
+                Here you’ll find all kinds of gifts at a reduced price. Check
+                out our limited edition gifts, men’s gift boxes, kids’ gifts and
+                many more.
+              </p>
+            </div>
+            <router-link to="/rewards" class="btn-container mt-5">
+              <v-btn
+                class="button uppercase white--text font-weight-bold"
+                elevation="2"
+                outlined
+                large
+              >
+                <div class="btn-text">rewards</div>
+                <i class="btn-icon fa-solid fa-gift"></i>
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script>
 export default {
   name: "CarouselSection",
-  data() {
-    return {
-      colors: [
-        "indigo",
-        "warning",
-        "pink darken-2",
-        "red lighten-1",
-        "deep-purple accent-4",
-      ],
-      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
-    };
-  },
+  // data() {},
   //   computed: {},
   //   methods: {},
   //   mounted: {},
